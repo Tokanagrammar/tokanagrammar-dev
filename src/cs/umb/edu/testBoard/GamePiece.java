9ef76d317d1e9e24bf50c9d7aa1cb812a47cfa6d
Matt <battlesignsusmc@gmail.com>
2013-03-03 23:28:05 -0500

    update

---
+++ src/cs/umb/edu/testBoard/GamePiece.java
@@ -0,0 +1,42 @@
+package cs.umb.edu.testBoard;
+
+import javafx.scene.Node;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+/**
+ * an actual "token" that is in a graphical form on the board.
+ * It seems practical to extend Node class from FX since it's 
+ * a lower-level dragable object.  THE GAME PIECES WILL NEED
+ * TO BE DRAGGABLE!
+ * 
+ * @author Matt
+ *
+ */
+public abstract class GamePiece extends Node{
+	
+	private FauxToken token;
+	private Image bgImage;
+	private ImageView imageView;
+	
+	
+	public GamePiece(FauxToken token){
+		this.token = token;
+		this.bgImage = getBGImage();
+	}
+	
+	
+	public Image getBGImage(){
+		if(token.type == "INTEGER"){
+			Image img = new Image("file:green.fw.png");
+			
+			this.imageView = new ImageView(img);
+			
+			return new Image("file:red_intBG.fw.png");
+		}
+		else
+			return new Image("file:greenStd.fw.png");
+	}
+
+
+}
---
+++ src/cs/umb/edu/tokanagrammar/Tokanagrammar.java
@@ -0,0 +1,50 @@
+package cs.umb.edu.tokanagrammar;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class Tokanagrammar extends Application{
+	
+	//static Group root = new Group();		//fix this to keep track of everything needing blurring! -mhs
+	
+    public static void main(String[] args) {
+        Application.launch(Tokanagrammar.class, (java.lang.String[]) null);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+    	
+        try {
+            AnchorPane page = (AnchorPane) FXMLLoader.load(Tokanagrammar.class.getResource("../../../../Tokanagrammar.fxml"));
+            Scene scene = new Scene(page);
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("Tokanagrammar 0.1");
+            primaryStage.show();
+            
+            //root.getChildren().add(page);	// -mhs this is causing roll-overs to stop working, but I'd like to save all layers here.
+            //ref BUG --- -mhs experimenting with pause blur here seems to work here, but not in controller where I want it.
+            //root.getChildren().get(0).effectProperty().set(new BoxBlur());  //-mhs
+        } catch (Exception ex) {
+            Logger.getLogger(Tokanagrammar.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    /**
+     * Getting the group might be helpful in doing any kind of
+     * effect for the background.  (pausing is the only thing
+     * that comes to mind now... [blurring])
+     * @return
+     */
+    public static Group getRootGroup(){
+    	return null;
+    	//return root; //-mhs
+    }
+
+}
